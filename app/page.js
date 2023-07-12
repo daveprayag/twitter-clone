ee5ef450e167e84cf7362d81091d80a4370f2b3f
daveprayag <prayagdave28@gmail.com>
2023-07-12 22:09:20 +0530

    Feed component and header

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
@@ -7,8 +8,10 @@ export default function Home() {
       <Sidebar />
 
       {/* Feed */}
-
+      <Feed />
       {/* Widgets */}
+
+      {/* Modal */}
     </main>
   );
 }
