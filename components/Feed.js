4fa9df9018e2c80b72e388a3d26350d8907fd846
daveprayag <prayagdave28@gmail.com>
2023-08-25 02:00:36 +0530

    Added fade in motion using framer motion

--- components/Feed.js
+++ components/Feed.js
@@ -3,6 +3,7 @@ import Input from "./Input";
 import Post from "./Post";
 import { useEffect, useState } from "react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { AnimatePresence, motion } from "framer-motion";
 import { db } from "../firebase";
 
 export default function Feed() {
@@ -27,9 +28,19 @@ export default function Feed() {
         </div>
       </div>
       <Input />
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post key={post.id} id={post.id} post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
