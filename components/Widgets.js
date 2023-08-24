c8ed60efb8236280ca4f3dcc3a8e66e1bd3cb485
daveprayag <prayagdave28@gmail.com>
2023-08-25 02:01:19 +0530

    Added fade-in motion using framer motion

--- components/Widgets.js
+++ components/Widgets.js
@@ -77,30 +77,6 @@ export default function Widgets({ newsResults, randomUsersResults }) {
             </motion.div>
           ))}
         </AnimatePresence>
-        {/* {randomUsersResults.slice(0, userNum).map((randomUser) => (
-          <div
-            key={randomUser.login.username}
-            className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200 transition duration-500 ease-out"
-          >
-            <img
-              className="rounded-full"
-              width="40"
-              src={randomUser.picture.thumbnail}
-              alt=""
-            />
-            <div className="truncate ml-4 leading-5">
-              <h4 className="font-bold hover:underline text-[14px] truncate">
-                {randomUser.login.username}
-              </h4>
-              <h5 className="text-[13px] text-gray-500 truncate">
-                {randomUser.name.first + " " + randomUser.name.last}
-              </h5>
-            </div>
-            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
-              Follow
-            </button>
-          </div>
-        ))} */}
         <button
           onClick={() => setUserNum(userNum + 2)}
           className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
